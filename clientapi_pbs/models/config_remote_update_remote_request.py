3d79be3b4ec15a1cabecb734eee7063a2f57511d
Bence Santha <bence@santha.eu>
2026-05-24 22:58:01 +0200

    chore: sync from pve-openapi@9deff0a
    fix: expand POSIX char classes + ship optional-dependencies block

--- clientapi_pbs/models/config_remote_update_remote_request.py
+++ clientapi_pbs/models/config_remote_update_remote_request.py
@@ -60,8 +60,8 @@ class ConfigRemoteUpdateRemoteRequest(BaseModel):
         if not isinstance(value, str):
             value = str(value)
 
-        if not re.match(r"^(?:(?:[^\s:/[:cntrl:]]+)@(?:[A-Za-z0-9_][A-Za-z0-9._\-]*)|(?:[^\s:/[:cntrl:]]+)@(?:[A-Za-z0-9_][A-Za-z0-9._\-]*)!(?:[A-Za-z0-9_][A-Za-z0-9._\-]*))$", value):
-            raise ValueError(r"must validate the regular expression /^(?:(?:[^\s:/[:cntrl:]]+)@(?:[A-Za-z0-9_][A-Za-z0-9._\-]*)|(?:[^\s:/[:cntrl:]]+)@(?:[A-Za-z0-9_][A-Za-z0-9._\-]*)!(?:[A-Za-z0-9_][A-Za-z0-9._\-]*))$/")
+        if not re.match(r"^(?:(?:[^\s:/\x00-\x1f\x7f]+)@(?:[A-Za-z0-9_][A-Za-z0-9._\-]*)|(?:[^\s:/\x00-\x1f\x7f]+)@(?:[A-Za-z0-9_][A-Za-z0-9._\-]*)!(?:[A-Za-z0-9_][A-Za-z0-9._\-]*))$", value):
+            raise ValueError(r"must validate the regular expression /^(?:(?:[^\s:/\x00-\x1f\x7f]+)@(?:[A-Za-z0-9_][A-Za-z0-9._\-]*)|(?:[^\s:/\x00-\x1f\x7f]+)@(?:[A-Za-z0-9_][A-Za-z0-9._\-]*)!(?:[A-Za-z0-9_][A-Za-z0-9._\-]*))$/")
         return value
 
 
@@ -74,8 +74,8 @@ class ConfigRemoteUpdateRemoteRequest(BaseModel):
         if not isinstance(value, str):
             value = str(value)
 
-        if not re.match(r"^[[:^cntrl:]]*$", value):
-            raise ValueError(r"must validate the regular expression /^[[:^cntrl:]]*$/")
+        if not re.match(r"^[^\x00-\x1f\x7f]*$", value):
+            raise ValueError(r"must validate the regular expression /^[^\x00-\x1f\x7f]*$/")
         return value
 
 
@@ -131,8 +131,8 @@ class ConfigRemoteUpdateRemoteRequest(BaseModel):
         if not isinstance(value, str):
             value = str(value)
 
-        if not re.match(r"^[[:^cntrl:]]*$", value):
-            raise ValueError(r"must validate the regular expression /^[[:^cntrl:]]*$/")
+        if not re.match(r"^[^\x00-\x1f\x7f]*$", value):
+            raise ValueError(r"must validate the regular expression /^[^\x00-\x1f\x7f]*$/")
         return value
 
 
